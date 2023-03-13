a9a997480589b4772cc208d782b57cb12c4e65d6
katkapsasky <kat_kapsasky@hotmail.com>
2023-03-13 16:41:04 +0000

    Create admin functionality to post recipe

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Recipe
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Recipe)
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'approved', 'posted_on')
+    search_fields = ['title', 'author']
+    list_filter = ('approved', 'posted_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('method')
--- recipe/models.py
+++ recipe/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-STATUS = ((0, "Awaiting Approval"), (1, "Approved"))
-
 
 class Recipe(models.Model):
     title = models.CharField(max_length=50, unique=True)
@@ -11,12 +9,11 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="recipe_post"
     )
-    total_time = models.IntegerField()
+    total_time = models.CharField(max_length=50)
     method = models.TextField()
     image = CloudinaryField('image', default='placeholder')
     recipe_description = models.CharField(max_length=200)
     posted_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(
         User, related_name='recipe_likes', blank=True
     )
