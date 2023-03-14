4f381f6220ecbaa5c47844010c36162b1e899022
katkapsasky <kat_kapsasky@hotmail.com>
2023-03-14 15:27:48 +0000

    Install allauth for user login and signup

--- cakesandbakes/urls.py
+++ cakesandbakes/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipe.urls'), name='recipe_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
