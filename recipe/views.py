340f84a48d78b53a724f175d9ebdbafa96fed2fb
katkapsasky <kat_kapsasky@hotmail.com>
2023-03-14 13:52:14 +0000

    Add functionality to view recipe post details

--- recipe/views.py
+++ recipe/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.views import generic
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import Recipe
 
 
 class RecipeList(generic.ListView):
@@ -8,3 +8,22 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(approved=True).order_by('-posted_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class RecipeDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(approved=True)
+        recipe = get_object_or_404(queryset, slug=slug)
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "liked": liked
+            },
+        )
