6f921590ce6eab50745555a7d52ab003e3cc8c20
katkapsasky <kat_kapsasky@hotmail.com>
2023-03-14 19:00:11 +0000

    Update recipe form

--- recipe/forms.py
+++ recipe/forms.py
@@ -1,17 +1,24 @@
-from .models import Recipe
 from django import forms
+from .models import Recipe, Category
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = "__all__"
 
 
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = (
-            'title',
-            'total_time',
-            'ingredient_name',
-            'metric',
-            'quantity',
-            'method',
-            'image',
-            'recipe_description',
+        fields = "__all__"
+        exclude = (
+            "author",
+            "posted_on",
+            "likes",
+            "approved",
         )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["slug"].widget = forms.HiddenInput()
