d9f4c0f135b1e315c4363a46b9b26c388017f4c9
katkapsasky <kat_kapsasky@hotmail.com>
2023-03-14 18:59:07 +0000

    Update recipe and category models

--- recipe/models.py
+++ recipe/models.py
@@ -3,19 +3,38 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 
+class Category(models.Model):
+    recipe_type = models.CharField(max_length=50, null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.recipe_type
+
+
 class Recipe(models.Model):
-    title = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    title = models.CharField(
+        max_length=50, unique=True, null=False, blank=False
+    )
+    slug = models.SlugField(
+        max_length=200, unique=True, null=False, blank=False
+    )
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="recipe_post"
     )
-    total_time = models.CharField(max_length=50)
-    ingredient_name = models.CharField(max_length=50, null=True)
-    metric = models.CharField(max_length=25, null=True)
-    quantity = models.IntegerField(null=True)
-    method = models.TextField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
+    total_time = models.CharField(max_length=50, null=False, blank=False)
+    ingredients = models.TextField(null=False, blank=False)
+    method = models.TextField(null=False, blank=False)
     image = CloudinaryField('image', default='placeholder')
-    recipe_description = models.CharField(max_length=200)
+    recipe_description = models.CharField(
+        max_length=200, null=False, blank=False
+    )
     posted_on = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(
         User, related_name='recipe_likes', blank=True
@@ -30,16 +49,3 @@ class Recipe(models.Model):
 
     def number_of_likes(self):
         return self.likes.count()
-
-
-class Category(models.Model):
-    recipe_type = models.CharField(max_length=50)
-    slug = models.SlugField(unique=True, null=True)
-    description = models.TextField(u'Description', blank=True, null=True)
-
-    class Meta:
-        verbose_name = u'Category'
-        verbose_name_plural = u'Categories'
-
-    def __unicode__(self):
-        return self.name
