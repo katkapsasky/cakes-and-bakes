95d4ab5a85cf0df1f609b195fb7527c2c1292fe0
katkapsasky <kat_kapsasky@hotmail.com>
2023-03-27 15:03:01 +0000

    Remove additional admin site

--- recipe/admin.py
+++ recipe/admin.py
@@ -17,20 +17,5 @@ class RecipeAdmin(admin.ModelAdmin):
         queryset.update(approved=True)
 
 
-class RecipeAdminSite(admin.AdminSite):
-    '''
-    Class based function to create additional admin site
-    for site administrators/managers
-    '''
-    site_header = "Recipe Post Admin"
-    site_title = "Recipe Post Admin Portal"
-    title = "Welcome to Cakes & Bakes"
-
-
-recipe_admin_site = RecipeAdminSite(name='recipe_admin')
-
-
-recipe_admin_site.register(Recipe)
-recipe_admin_site.register(Category)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Category)
