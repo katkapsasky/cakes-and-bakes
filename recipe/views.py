28fd7a614adfab987daf005b7ef659a7fbf0406d
katkapsasky <kat_kapsasky@hotmail.com>
2023-03-27 15:02:52 +0000

    Add liked recipes page to show users liked recipes

--- recipe/views.py
+++ recipe/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.views import generic, View
 from django.contrib import messages
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
-from .models import Recipe
+from .models import Recipe, Category
 from .forms import RecipeForm, CategoryForm
 
 
@@ -109,6 +110,10 @@ def delete_recipe(request, slug):
         )
         return redirect('home')
     recipe.delete()
+    messages.success(
+        request,
+        'Recipe successfully deleted!'
+    )
     return redirect('home')
 
 
@@ -120,23 +125,104 @@ class RecipeLikes(View):
         recipe = get_object_or_404(Recipe, slug=slug)
         if recipe.likes.filter(id=request.user.id).exists():
             recipe.likes.remove(request.user)
+            messages.success(
+                request,
+                'Recipe successfully unliked.'
+            )
         else:
             recipe.likes.add(request.user)
-
+            messages.success(
+                request,
+                'Recipe successfully liked.'
+            )
         return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
 
 
+def manage_categories(request):
+    """
+    Manage categories as superuser
+    """
+    if not request.user.is_superuser:
+        # user is not superuser; take to home page
+        messages.error(request, 'Access denied. Invalid permissions.')
+        return redirect(reverse('home'))
+    categories = Category.objects.all().order_by('recipe_type')
+    template = 'manage_categories.html'
+    context = {
+        'categories': categories,
+    }
+    return render(request, template, context)
+
+
 def post_category(request):
+    '''
+    Function to post a new category type
+    '''
+    if not request.user.is_superuser:
+        # user is not superuser; take to home page
+        messages.error(request, 'Access denied. Invalid permissions.')
+        return redirect(reverse('home'))
     category_form = CategoryForm(request.POST or None)
     if request.method == "POST":
         if category_form.is_valid():
             category_form.save()
             messages.success(request, 'Category successfully added!')
-            return redirect('home')
+            return redirect('manage_categories')
 
         messages.error(request, 'An error has occurred, please try again.')
     template = 'post_category.html'
     context = {
         'category_form': category_form,
+    }
+    return render(request, template, context)
+
+
+def edit_category(request, id):
+    '''
+    Function to edit an existing category type
+    '''
+    if not request.user.is_superuser:
+        # user is not superuser; take to home page
+        messages.error(request, 'Access denied. Invalid permissions.')
+        return redirect(reverse('home'))
+    category = get_object_or_404(Category, id=id)
+    category_form = CategoryForm(request.POST or None, instance=category)
+    if request.method == "POST":
+        if category_form.is_valid():
+            category_form.save()
+            messages.success(request, 'Category successfully edited!')
+            return redirect('manage_categories')
+
+        messages.error(request, 'An error has occurred, please try again.')
+    template = 'edit_category.html'
+    context = {
+        'category': category,
+        'category_form': category_form,
+    }
+    return render(request, template, context)
+
+
+def delete_category(request, id):
+    '''
+    Function to delete an existing category type
+    '''
+    if not request.user.is_superuser:
+        # user is not superuser; take to home page
+        messages.error(request, 'Access denied. Invalid permissions.')
+        return redirect(reverse('home'))
+    category = get_object_or_404(Category, id=id)
+    category.delete()
+    messages.success(request, 'Category successfully deleted!')
+    return redirect('manage_categories')
+
+
+def liked_recipes(request):
+    '''
+    A page dedicated to the user's liked/saved recipes.
+    '''
+    recipes = Recipe.objects.filter(likes=request.user)
+    template = 'liked_recipes.html'
+    context = {
+        'recipes': recipes,
     }
     return render(request, template, context)
